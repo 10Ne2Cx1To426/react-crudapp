2ffbd987e0295f414cdb94e36bc7554faeb8e762
Sena <73450328+10Ne2Cx1To426@users.noreply.github.com>
2021-05-11 08:31:19 +0900

    edit

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -12,6 +12,11 @@ class EventsShow extends Component {
     this.onDeleteClick = this.onDeleteClick.bind(this);
   }
 
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    if (id) this.props.getEvent(id);
+  }
+
   renderField(field) {
     const {
       input,
@@ -28,7 +33,7 @@ class EventsShow extends Component {
   }
 
   async onSubmit(values) {
-    // await this.props.postEvents(values);
+    await this.props.putEvent(values);
     this.props.history.push("/");
   }
 
@@ -39,7 +44,7 @@ class EventsShow extends Component {
   }
 
   render() {
-    const { handleSubmit, pristine, submitting } = this.props;
+    const { handleSubmit, pristine, submitting, invalid } = this.props;
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div>
@@ -63,7 +68,7 @@ class EventsShow extends Component {
           <input
             type="submit"
             value="Submit"
-            disabled={pristine || submitting}
+            disabled={pristine || submitting || invalid}
           />
           <Link to="/">Cancel</Link>
           <Link to="/" onClick={this.onDeleteClick}>
@@ -83,9 +88,18 @@ const validate = (values) => {
   return errors;
 };
 
-const mapDispachToProps = { deleteEvent };
+const mapDispachToProps = { deleteEvent, getEvent, putEvent };
+
+const mapStateToProps = (state, ownProps) => {
+  const event = state.events[ownProps.match.params.id];
+  return { initialValues: event, event };
+};
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispachToProps
-)(reduxForm({ validate, form: "eventShowForm" })(EventsShow));
+)(
+  reduxForm({ validate, form: "eventShowForm", enableReinitialize: true })(
+    EventsShow
+  )
+);
